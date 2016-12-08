fe37f2de5921fd1af28bb188337e6edd060f7df6
martreba <martreba@greeny.cs.tlu.ee>
2016-12-08 14:55:56 +0200

    xd

--- pages/Legs.php
+++ pages/Legs.php
@@ -3,8 +3,13 @@
 <div class="container">
 	
 	<div class="col col-xs-offset-1 col-xs-7">
-		<h2>Pealkiri</h2>
-		<p>Sisu</p>
+		<h2>Legs</h2>
+		<p>
+		The anterior muscles, such as the quadriceps femoris, iliopsoas, and sartorius, work as a group to flex the thigh at the hip and extend the leg at the knee. Posterior muscles, such as the hamstrings and gluteus maximus, produce the opposite motion – extension of the thigh at the hip and flexion of the leg at the knee. Lateral muscles, such as the gluteus medius, abduct the thigh at the hip while the medial groin muscles adduct the thigh. All of these muscle groups provide powerful contractions to propel the body while making fine adjustments to maintain the body’s posture and balance.
+
+		Located inferior to the knee are a number of muscles that move the ankle, foot, and toes. The calf muscles, including the gastrocnemius and soleus, join to form the strong calcaneal (Achilles) tendon of the heel and attach to the calcaneus bone in the heel. These muscles contract to plantar flex the foot – such as when standing on your tiptoes – and flex the toes. Shin muscles, such as the tibialis anterior and extensor digitorum longus, dorsiflex the foot and extend the toes. The muscles of the calf also work subtly to stabilize the ankle joint and foot and to maintain the body’s balance.
+		
+		</p>
 	</div>
 </div>
 <?php require("../footer.php"); ?>
--- pages/Shoulders.php
+++ pages/Shoulders.php
@@ -2,9 +2,27 @@
 
 <div class="container">
 	
-	<div class="col col-xs-offset-1 col-xs-7">
-		<h2>Pealkiri</h2>
-		<p>Sisu</p>
+	<div class="col col-xs-offset-1 col-xs-7 ">
+		<h2>Shoulder</h2>
+		<p>
+		<img src="https://www.shoulderdoc.co.uk/images/uploaded/body2.jpg" alt="shoulder muscles"><br>
+			The muscles of the shoulder either connect the scapula and clavicle to the trunk, or connect the clavicle, scapula and body wall to the proximal (top) end of the humerus. The trapezius, levator scapulae, and rhomboids originate from the base of the skull and/or spine and connect the scapula and clavicle to the trunk of the body. The pectoralis major, pectoralis minor, latissimus dorsi, teres major and deltoid connect to the proximal end of the humerus and anchor it to the body.
+The most important shoulder muscles are the four rotator cuff muscles - the subscapularis, supraspinatus, infraspinatus and teres minor muscles - which connect the scapula to the humerus and provide support for the glenohumeral joint. 
+Muscles of the arm that enter into the shoulder complex are separated into anterior (flexor) and posterior (extensor) compartments. These include biceps brachii, triceps brachii and coracobrachialis.
+			
+			
+			
+			
+		
+		</p>
+		<h3>Exercises</h3>
+		<p>
+		<img src="http://www.bodybuilding.com/exercises/exerciseImages/sequences/74/Male/m/74_1.jpg" alt="beginning">
+		<img src="http://www.bodybuilding.com/exercises/exerciseImages/sequences/74/Male/m/74_2.jpg" alt="start">
+			<br>
+			Machine Shoulder (Military) Press <br>
+			3 sets, 12 reps (60-90 seconds rest)
+		</p>
 	</div>
 </div>
 <?php require("../footer.php"); ?>
