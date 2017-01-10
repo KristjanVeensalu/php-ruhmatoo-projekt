77b5103639d432a56ba6283c984d2339c186ed68
krisveen <krisveen@greeny.cs.tlu.ee>
2017-01-10 17:08:00 +0200

    Added back files

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require("../../../config.php");
-	
-	session_start();
-	
-	$database = "if16_doitbodyweight";
-	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
-	
-?>
