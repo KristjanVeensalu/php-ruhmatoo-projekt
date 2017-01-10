f79ef989b5a50c8985f5ccd3eba79c08f4f8f1eb
KristjanVeensalu <KristjanVeensalu@users.noreply.github.com>
2017-01-10 17:12:12 +0200

    Add files via upload
    ADded back files

---
+++ php-ruhmatoo-projekt/pages/Abs.php
@@ -0,0 +1,32 @@
+
+
+<?php require "../header.php"; 
+?>
+
+<div class="container">
+	
+	<div class="col col-xs-offset-1 col-xs-7">
+		<h2>The Abdomen</h2>
+		<br></br>
+		<img src = http://www.findhomeremedy.com/wp-content/uploads/2016/01/Opening-Of-The-Abdomen.jpg alt = "The Abdomen" />
+		<br></br>
+		<p>The abdomen (less formally called the belly, stomach, tummy or midriff) constitutes the part of the body between the thorax (chest) and pelvis, in humans and in other vertebrates.
+			The region occupied by the abdomen is termed the abdominal cavity. 
+			The abdomen stretches from the thorax to the pelvis. 
+			The boundary of the abdominal cavity is the abdominal wall in the front and the peritoneal surface at the rear.
+		<h3>The Muscles</h3>
+			<br></br>
+			<img src = http://leanmuscleproject.com/wp-content/uploads/2014/09/abdominal-muscles-diagram.jpg alt = "The Muscles" />
+			<br></br>
+		<p>The four main abdominal muscle groups that combine to completely cover the internal organs include:
+			<br></br>
+			Transversus abdominis – the deepest muscle layer. Its main roles are to stabilise the trunk and maintain internal abdominal pressure
+			<br></br>
+			Rectus abdominis – slung between the ribs and the pubic bone at the front of the pelvis. When contracting, this muscle has the characteristic bumps or bulges that are commonly called ‘the six pack’. The main function of the rectus abdominis is to move the body between the ribcage and the pelvis
+			<br></br>
+			External oblique muscles – these are on each side of the rectus abdominis. The external oblique muscles allow the trunk to twist, but to the opposite side of whichever external oblique is contracting. For example, the right external oblique contracts to turn the body to the left
+			<br></br>
+			Internal oblique muscles – these flank the rectus abdominis and are located just inside the hipbones. They operate in the opposite way to the external oblique muscles. For example, twisting the trunk to the left requires the left side internal oblique and the right side external oblique to contract together.</p>
+	</div>
+</div>
+<?php require("../footer.php"); ?>
---
+++ php-ruhmatoo-projekt/pages/Chest.php
@@ -0,0 +1,10 @@
+<?php require "../header.php"; ?>
+
+<div class="container">
+	
+	<div class="col col-xs-offset-1 col-xs-7">
+		<h2>Pealkiri</h2>
+		<p>Sisu</p>
+	</div>
+</div>
+<?php require("../footer.php"); ?>
