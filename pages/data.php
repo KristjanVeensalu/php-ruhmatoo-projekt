e4f07ea4a0b40a509589bc17a8aefc00915a1642
krisveen <krisveen@greeny.cs.tlu.ee>
2017-01-10 16:23:03 +0200

    Fixed classes and added functions for data entry

--- pages/data.php
+++ pages/data.php
@@ -1,8 +1,6 @@
 <?php
-require ("../functions.php");
-
-require("../class/User.class.php");
-$User = new User($mysqli);
+var_dump ($_POST);
+require ("functions.php");
 
 if (!isset($_SESSION["userId"])) {
 	header("Location: esileht.php");
@@ -11,29 +9,46 @@ if (!isset($_SESSION["userId"])) {
 
 	$exerciseError = "*"; 
 	$repError = "*";
-
-
+	$exercise = " ";
+	$reps = " ";
+	$selectionError = "*";
+	$selection = "";
+	
 if (isset ($_POST["exercise"]))
 		{
 		if( empty ($_POST["exercise"])){
 			$exerciseError = "Field must be filled";}
 		}
-	if (isset ($_POST["reps"]))
+if (isset ($_POST["reps"]))
 		{
 		if( empty ($_POST["reps"])){
 			$repError = "Field must be filled";}
 		}
+if (isset ($_POST["selection"]))
+			{
+		if (empty ($_POST[selection])){
+			$selection = "Field must be filled";}	
+			}
 		
-		if($exerciseError == "*"  &&
-			$repError == "*"	&&
-			isset($_POST["exercise"]) &&
-			isset($_POST["reps"]) &&
-			$exercise = $_POST["exercise"];
-			$reps = $_POST["reps"];
+		
+		
+		
+if($exerciseError == "*"  &&
+	$repError == "*"	&&
+	isset($_POST["exercise"]) &&
+	isset($_POST["reps"]))
+	{$exercise = $_POST["Exercise"];
+		$reps = $_POST["Reps"];
+			$selection = $_POST["Selection"];
+	}
+				
+					
+				
 
 
-
-
+if($selection = "Abs"){
+			$data->dataentryAbs ($Helper->cleanInput($exercise), $Helper->cleanInput($reps));
+}
 
 
 
@@ -61,13 +76,22 @@ if (isset ($_POST["exercise"]))
 	<div class="col-md-2"
 	<br>
 		<form method="POST">
+						<select name = "Selection"> <?php echo $selectionError ?>
+							<option value="Abs">Abs</option>
+							<option value="Back">Back</option>
+							<option value="Chest">Chest</option>
+							<option value="Core">Core</option>
+							<option value="Legs">Legs</option>
+							<option value="Shoulders">Shoulders</option>
+						</select>
+					<br></br>
 				<div class="input-group input-group-sm">
 					<div class="row">
-						<input class="form-control" name="exercise" placeholder="Exercise" type="text"> <?php echo $exerciseError ?>
+						<input class="form-control" name="Exercise" placeholder="Exercise" type="text"> <?php echo $exerciseError ?>
 					</div>
 					
 					<div class="row">
-						<input class="form-control" name="reps" placeholder="Reps" type="number"> <?php echo $repError ?>
+						<input class="form-control" name="Reps" placeholder="Reps" type="number"> <?php echo $repError ?>
 					</div>
 					
 					<div class="row">
@@ -81,11 +105,11 @@ if (isset ($_POST["exercise"]))
 		<form method="POST">
 				<div class="input-group input-group-sm">
 					<div class="row">
-							<input class="form-control" name="food" placeholder="Toit" type="username">
+							<input class="form-control" name="Food" placeholder="Food" type="username">
 					</div>
 					
 					<div class="row">
-						<input class="form-control" name="calories" placeholder="Kalorite kogus" type="text">
+						<input class="form-control" name="Calories" placeholder="Calories" type="text">
 					</div>
 					<br>
 					
